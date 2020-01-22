0b1eb88eb4a1eadc9215ae9ab28563d6b4458e9d
Cristian Mello <cristianc.mello@gmail.com>
2020-01-22 17:49:40 +0000

    Updates...

--- handler.js
+++ handler.js
@@ -3,15 +3,36 @@
 const fs = require('fs')
 
 module.exports.loadIndex = async event => {
-  const indexPageHtml = fs.readFileSync('index.html').toString();
+  //const indexPageHtml = fs.readFileSync('index.html').toString();
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: indexPageHtml,
-  };
+  try {
+    const showdown = require('showdown'),
+      converter = new showdown.Converter(),
+      text = fs.readFileSync(event.path.replace(/^\/|\/$/g, '')).toString(),
+      html = converter.makeHtml(text);
 
-  return response
+    const response = {
+      statusCode: 200,
+      body: { message: 'hello' },
+    };
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: html
+    };
+  }
+  catch (error) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+          message: error
+        },
+        null,
+        2
+      ),
+    };
+  }
 };
